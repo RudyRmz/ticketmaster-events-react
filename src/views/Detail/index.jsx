e32ff1d894c3f21b2d9972bc5a7eef1d7b30450a
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2024-01-25 16:38:02 -0600

    se trabajo con el fetch del componente Detail para obtener los datos del evento al que se le dio click

--- src/views/Detail/index.jsx
+++ src/views/Detail/index.jsx
@@ -1,3 +1,30 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function Detail() {
+  const { eventId } = useParams();
+  const [eventData, setEventData] = useState({});
+  const [error, setError] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchEventData = async () => {
+      try {
+        const response = await fetch(
+          `https://app.ticketmaster.com/discovery/v2/events/${eventId}?apikey=37wchGrCGX0FRh41utv4Ji8AgYWeGqZ3`
+        );
+        const data = await response.json();
+        //console.log(data);
+        setEventData(data);
+        setIsLoading(false);
+      } catch (error) {
+        setEventData({});
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+    fetchEventData();
+  }, []);
+  //console.log(eventData);
   return <div>Detail</div>;
 }
