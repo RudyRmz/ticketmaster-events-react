ec3fd363e0ce6637a27d4738bbac3dcd0f73a50e
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2024-02-26 17:56:01 -0600

    se agrego funcionalidad like unlike

--- src/components/Events/components/EventItem/EventItem.jsx
+++ src/components/Events/components/EventItem/EventItem.jsx
@@ -1,14 +1,32 @@
 import { Link } from "react-router-dom";
 
+import HeartFilled from "../../../../assets/hearth-filled.png";
+import HeartUnfilled from "../../../../assets/hearth-unfilled.png";
+import useLikedEvents from "../../../../hooks/useLikedEvents";
+
 export default function EventItem({ info, name, image, onEventClick, id }) {
+  const { isEventLiked, toggleEventLike } = useLikedEvents(id);
   const handleSeeMoreClick = (evt) => {
     evt.stopPropagation();
     onEventClick(id);
     //console.log("diste click al boton");
   };
+
+  const handleHearthClick = () => {
+    toggleEventLike();
+  };
+
   return (
-    <div className=" flex m-5">
-      <img src={image} alt={name} width={200} height={200} />
+    <div className=" m-5">
+      <div className=" relative">
+        <img
+          src={isEventLiked ? HeartFilled : HeartUnfilled}
+          alt="Hearth button"
+          className=" absolute w-7 top-3 left-3 cursor-pointer"
+          onClick={handleHearthClick}
+        />
+        <img src={image} alt={name} className=" w-52 h-52 rounded-md" />
+      </div>
       <div className=" flex flex-col items-start ml-4">
         <h4 className=" font-bold text-xl ">{name}</h4>
         <p className=" text-left line-clamp-5">{info}</p>
